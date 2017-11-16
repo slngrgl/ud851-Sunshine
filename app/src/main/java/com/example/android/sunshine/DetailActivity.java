c83b768943f88934cdb5f7b4ba01fdf5f4a279e1
Selin Görgülü <selin.gorgulu@nesine.com>
2017-11-16 14:19:46 +0300

    - TODOs are done

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,30 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
-    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private String forecast;
+    private TextView weatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        weatherTextView = (TextView) findViewById(R.id.weather_textview);
+
+        Intent intent = getIntent();
+
+        // COMPLETED (2) Display the weather forecast that was passed from MainActivity
+        if (intent != null) {
+            if (intent.hasExtra(Intent.EXTRA_TEXT)) {
+                forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+                weatherTextView.setText(forecast);
+            }
+        }
     }
 }
